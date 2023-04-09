bcbf53826c1520c4cf3ab354dec4b58bceff64dc
labhk <labh.k2003@gmail.com>
2023-04-09 18:01:52 +0530

    repeat

--- index.js
+++ index.js
@@ -11,16 +11,13 @@ const openai = new OpenAIApi(new Configuration({
     apiKey: process.env.API_KEY
 }))
 
-
+// setup server
 const app = express();
 app.use(bodyParser.json());
-const corsOptions = {
-    origin: ['https://course-verse.netlify.app/', 'http://localhost:5173'],
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  };
-  
-  app.use(cors(corsOptions));
-
+app.use(
+    cors({
+        origin:"*",
+}));
 
 app.post("/chat", async (req,res) => {
 
@@ -37,7 +34,7 @@ app.post("/chat", async (req,res) => {
 
 })
 
-const port = 8080
+const port = 8081
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 })
